8a462182d44d7b8f543f6c3a48755caf4881c01f
Luke Hansell <luke.hansell@holidayextras.com>
2016-01-15 16:16:15 +0000

    adding content thingies for google

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -9,25 +9,34 @@ export default class BlogItem extends Component {
 	render() {
 		return (
 			<Panel className="blogItem" >
-				<h2>{this.props.title}</h2>
-				<div dangerouslySetInnerHTML={{__html: this.props.content}} />
-				{this.props.date && (
-					<small>Posted on: {this.props.date.toDateString()}</small>
-				)}
-				<br/>
-				<a href={`http://twitter.com/home?status=@luke_hansell re:${this.props.title}`} target="_blank">Any comments or questions?</a>
+				<article itemscope itemtype="http://schema.org/NewsArticle">
+					<h2 itemprop="headline">{this.props.title}</h2>
+					<div dangerouslySetInnerHTML={{__html: this.props.content}} />
+					{this.props.date && (
+						<small>Posted on: {this.props.date.toDateString()}</small>
+					)}
 
-				{this.props.tags.length > 0 && (
-					<ul className="tags">
-						{this.props.tags.map( (tag, i) => {
-							return (
-								<li key={i}>
-									<Link to="/" query={{tag}}>{tag}</Link>
-								</li>
-							)
-						})}
-					</ul>
-				)}
+					<br/>
+
+					<a href={`http://twitter.com/home?status=@luke_hansell re:${this.props.title}`} target="_blank">Any comments or questions?</a>
+
+					<br/>
+
+					<a href={`http://lukehansell.co.uk/#/posts/${this.props.slug}`}>direct link to article</a>
+
+					{this.props.tags.length > 0 && (
+						<ul className="tags">
+							{this.props.tags.map( (tag, i) => {
+								return (
+									<li key={i}>
+										<Link to="/" query={{tag}}>{tag}</Link>
+									</li>
+								)
+							})}
+						</ul>
+					)}
+
+				</article>
 			</Panel>
 		)
 	}
